3b5fd718d766755922c97efbe0a33f480d1fadbc
Vesta-Nassone <vestanassone@gmail.com>
2022-01-29 17:01:02 +0200

    Reformat the file.

--- imdb/templates/movie/views.py
+++ imdb/templates/movie/views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import YearArchiveView
+from .models import Movie, MovieLinks
 
 # Create your views here.
-from django.views.generic import ListView , DetailView
-from django.views.generic.dates import YearArchiveView
-from .models import Movie , MovieLinks
-
-
-
 
 
 class HomeView(ListView):
     model = Movie
     template_name = 'movie/home.html'
 
-    def get_context_data(self , **kwargs):
-        context = super(HomeView , self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
         context['top_rated'] = Movie.objects.filter(status='TR')
         context['most_watched'] = Movie.objects.filter(status='MW')
-        context['recently_added'] = Movie.objects.filter(status='RA') 
+        context['recently_added'] = Movie.objects.filter(status='RA')
         return context
 
 
@@ -30,15 +26,16 @@ class MovieDetail(DetailView):
     model = Movie
 
     def get_object(self):
-        object = super(MovieDetail , self).get_object()
+        object = super(MovieDetail, self).get_object()
         object.views_count += 1
         object.save()
         return object
 
-    def get_context_data(self , **kwargs):
-        context = super(MovieDetail , self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(MovieDetail, self).get_context_data(**kwargs)
         context['links'] = MovieLinks.objects.filter(movie=self.get_object())
-        context['related_movies'] = Movie.objects.filter(category=self.get_object().category)#.order_by['created'][0:6]
+        context['related_movies'] = Movie.objects.filter(
+            category=self.get_object().category)  # .order_by['created'][0:6]
         return context
 
 
@@ -50,11 +47,10 @@ class MovieCategory(ListView):
         self.category = self.kwargs['category']
         return Movie.objects.filter(category=self.category)
 
-    def get_context_data(self , **kwargs):
-        context = super(MovieCategory , self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(MovieCategory, self).get_context_data(**kwargs)
         context['movie_category'] = self.category
         return context
-
 
 
 class MovieLanguage(ListView):
@@ -65,12 +61,10 @@ class MovieLanguage(ListView):
         self.language = self.kwargs['lang']
         return Movie.objects.filter(language=self.language)
 
-    def get_context_data(self , **kwargs):
-        context = super(MovieLanguage , self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(MovieLanguage, self).get_context_data(**kwargs)
         context['movie_language'] = self.language
         return context
-
-
 
 
 class MovieSearch(ListView):
@@ -81,11 +75,12 @@ class MovieSearch(ListView):
         query = self.request.GET.get('query')
         if query:
             object_list = self.model.objects.filter(title__icontains=query)
-        
+
         else:
             object_list = self.model.objects.none()
 
         return object_list
+
 
 class MovieYear(YearArchiveView):
     queryset = Movie.objects.all()
@@ -94,4 +89,3 @@ class MovieYear(YearArchiveView):
     allow_future = True
 
     print(queryset)
-        
